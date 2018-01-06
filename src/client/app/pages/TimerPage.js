6215212da45d9a81780d006a7959d4e8a0af5f98
Daniel Lundell <daniel.lundell@videocent.com>
2018-01-06 18:53:05 +0100

    Added boilerplate for new Pages

---
+++ src/client/app/pages/TimerPage.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Container} from 'semantic-ui-react';
+import RestrictedPageBase from 'pages/RestrictedPageBase';
+import Drawer from 'components/Drawer';
+import Footer from 'components/Footer';
+import HeaderBox from 'components/HeaderBox';
+
+export default class TimerPage extends RestrictedPageBase {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+
+  }
+
+  componentDidMount() {
+    this.mounted = true;
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+
+  renderContactInformation() {
+    return (
+      <p> Hello there, welcome to timewizard!</p>
+    );
+  }
+
+
+  render() {
+    return (
+      <div className="fullHeight">
+        <Drawer />
+        <div className='mainContent'>
+          <HeaderBox title="Timer" />
+          <Container>
+              {this.renderContactInformation()}
+          </Container>
+          <Footer />
+        </div>
+      </div>
+    );
+  }
+
+}
